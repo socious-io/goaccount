e4191fdb89ade02962e84d2ac9e5214a0d7fd2a2
Mohammad Hosein Balkhani <mohammadhb25@gmail.com>
2025-03-08 18:33:07 +0330

    feat: add organizations chore: some code refactor

---
+++ organization.go
@@ -0,0 +1,120 @@
+package goaccount
+
+import (
+	"encoding/json"
+)
+
+func GetAllOrganizations(organization interface{}) error {
+	response, err := Request(RequestOptions{
+		Endpoint: endpoint("organizations"),
+		Method:   MethodGet,
+	})
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(response, organization); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t SessionToken) GetMyOrganizations(organization interface{}) error {
+	response, err := Request(RequestOptions{
+		Endpoint: endpoint("organizations/my"),
+		Method:   MethodGet,
+		Headers: map[string]string{
+			"Authorization": bearer(t.AccessToken),
+		},
+	})
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(response, organization); err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetOrganization(organizationId string, organization interface{}) error {
+	response, err := Request(RequestOptions{
+		Endpoint: endpoint("organizations/%s", organizationId),
+		Method:   MethodGet,
+	})
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(response, organization); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t SessionToken) CreateOrganization(organizationId string, organization interface{}) error {
+	response, err := Request(RequestOptions{
+		Endpoint: endpoint("organizations"),
+		Method:   MethodPost,
+		Body:     organization,
+		Headers: map[string]string{
+			"Authorization": bearer(t.AccessToken),
+		},
+	})
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(response, organization); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t SessionToken) UpdateOrganization(organizationId string, organization interface{}) error {
+	response, err := Request(RequestOptions{
+		Endpoint: endpoint("organizations"),
+		Method:   MethodPost,
+		Body:     organization,
+		Headers: map[string]string{
+			"Authorization": bearer(t.AccessToken),
+		},
+	})
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(response, organization); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t SessionToken) AddMemberToOrganization(organizationId string, userId string, organization interface{}) error {
+	response, err := Request(RequestOptions{
+		Endpoint: endpoint("organizations/%s/members/%s", organizationId, userId),
+		Method:   MethodPost,
+		Headers: map[string]string{
+			"Authorization": bearer(t.AccessToken),
+		},
+	})
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(response, organization); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t SessionToken) RemoveMemberFromOrganization(organizationId string, userId string, organization interface{}) error {
+	response, err := Request(RequestOptions{
+		Endpoint: endpoint("organizations/%s/members/%s", organizationId, userId),
+		Method:   MethodDelete,
+		Headers: map[string]string{
+			"Authorization": bearer(t.AccessToken),
+		},
+	})
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(response, organization); err != nil {
+		return err
+	}
+	return nil
+}
