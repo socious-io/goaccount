7719b14291cbe1701120be65493604736f024d9d
Ehsan Mahmoudi <me@e-mahmoudi.me>
2025-04-18 09:06:35 +0330

    add impact point

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/socious-io/goaccount
 go 1.22.5
 
 require github.com/golang-jwt/jwt/v5 v5.2.2
+
+require github.com/google/uuid v1.6.0 // indirect
--- user.go
+++ user.go
@@ -2,7 +2,19 @@ package goaccount
 
 import (
 	"encoding/json"
+
+	"github.com/google/uuid"
 )
+
+type ImpactPoint struct {
+	UserID              uuid.UUID        `json:"user_id" form:"user_id" validate:"required"`
+	TotalPoints         int              `json:"total_points" form:"total_points"`
+	SocialCause         string           `json:"social_cause" form:"social_cause"`
+	SocialCauseCategory string           `json:"social_cause_category" form:"social_cause_category"`
+	Type                string           `json:"type" form:"type" validate:"required,oneof=WORKSUBMIT DONATION VOLUNTEER OTHER"`
+	AccessID            *uuid.UUID       `json:"access_id" form:"access_id"`
+	Meta                *json.RawMessage `json:"meta" form:"meta"`
+}
 
 // Get User profile base on access token given
 func (t *SessionToken) GetUserProfile(user interface{}) error {
@@ -73,6 +85,26 @@ func ChangeUserStatus(status string, user interface{}) error {
 		return err
 	}
 	if err := json.Unmarshal(response, user); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ip ImpactPoint) AddImpactPoint() error {
+	data, _ := json.Marshal(ip)
+	body := map[string]any{}
+
+	json.Unmarshal(data, &body)
+
+	body["client_id"] = config.ID
+	body["client_secret"] = config.Secret
+
+	_, err := Request(RequestOptions{
+		Endpoint: endpoint("impact-points"),
+		Method:   MethodPut,
+		Body:     body,
+	})
+	if err != nil {
 		return err
 	}
 	return nil
