b282d1a761b3db584f16946faa6ed4b6173ca5ce
Ehsan Mahmoudi <me@e-mahmoudi.me>
2025-08-16 21:58:35 +0330

    create user go convention

--- types.go
+++ types.go
@@ -40,50 +40,6 @@ type Media struct {
 	CreatedAt  time.Time `db:"created_at" json:"created_at"`
 }
 
-type User struct {
-	ID              uuid.UUID `db:"id" json:"id"`
-	Username        string    `db:"username" json:"username"`
-	Password        *string   `db:"password" json:"-"`
-	PasswordExpired bool      `db:"password_expired" json:"password_expired"`
-
-	Status StatusType `db:"status" json:"status"`
-
-	Email     string  `db:"email" json:"email"`
-	EmailText *string `db:"email_text" json:"email_text"`
-	Phone     *string `db:"phone" json:"phone"`
-
-	FirstName         *string `db:"first_name" json:"first_name"`
-	LastName          *string `db:"last_name" json:"last_name"`
-	Mission           *string `db:"mission" json:"mission"`
-	Bio               *string `db:"bio" json:"bio"`
-	DescriptionSearch *string `db:"description_search" json:"description_search"`
-
-	City              *string `db:"city" json:"city"`
-	Country           *string `db:"country" json:"country"`
-	Address           *string `db:"address" json:"address"`
-	GeonameId         *int64  `db:"geoname_id" json:"geoname_id"`
-	MobileCountryCode *string `db:"mobile_country_code" json:"mobile_country_code"`
-	ImpactPoints      *int    `db:"impact_points" json:"impact_points"`
-
-	AvatarID   *uuid.UUID     `db:"avatar_id" json:"avatar_id"`
-	Avatar     *Media         `db:"-" json:"avatar"`
-	AvatarJson types.JSONText `db:"avatar" json:"-"`
-
-	CoverID   *uuid.UUID     `db:"cover_id" json:"cover_id"`
-	Cover     *Media         `db:"-" json:"cover"`
-	CoverJson types.JSONText `db:"cover" json:"-"`
-
-	ReferredBy *uuid.UUID `db:"referred_by" json:"referred_by"`
-
-	IdentityVerifiedAt *time.Time `db:"identity_verified_at" json:"identity_verified_at"`
-	EmailVerifiedAt    *time.Time `db:"email_verified_at" json:"email_verified_at"`
-	PhoneVerifiedAt    *time.Time `db:"phone_verified_at" json:"phone_verified_at"`
-
-	CreatedAt time.Time  `db:"created_at" json:"created_at"`
-	UpdatedAt time.Time  `db:"updated_at" json:"updated_at"`
-	DeletedAt *time.Time `db:"deleted_at" json:"deleted_at"`
-}
-
 type Organization struct {
 	ID          uuid.UUID `db:"id" json:"id"`
 	Shortname   string    `db:"shortname" json:"shortname"`
--- user.go
+++ user.go
@@ -2,9 +2,55 @@ package goaccount
 
 import (
 	"encoding/json"
+	"time"
 
 	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx/types"
 )
+
+type User struct {
+	ID              uuid.UUID `db:"id" json:"id"`
+	Username        string    `db:"username" json:"username"`
+	Password        *string   `db:"password" json:"-"`
+	PasswordExpired bool      `db:"password_expired" json:"password_expired"`
+
+	Status StatusType `db:"status" json:"status"`
+
+	Email     string  `db:"email" json:"email"`
+	EmailText *string `db:"email_text" json:"email_text"`
+	Phone     *string `db:"phone" json:"phone"`
+
+	FirstName         *string `db:"first_name" json:"first_name"`
+	LastName          *string `db:"last_name" json:"last_name"`
+	Mission           *string `db:"mission" json:"mission"`
+	Bio               *string `db:"bio" json:"bio"`
+	DescriptionSearch *string `db:"description_search" json:"description_search"`
+
+	City              *string `db:"city" json:"city"`
+	Country           *string `db:"country" json:"country"`
+	Address           *string `db:"address" json:"address"`
+	GeonameId         *int64  `db:"geoname_id" json:"geoname_id"`
+	MobileCountryCode *string `db:"mobile_country_code" json:"mobile_country_code"`
+	ImpactPoints      *int    `db:"impact_points" json:"impact_points"`
+
+	AvatarID   *uuid.UUID     `db:"avatar_id" json:"avatar_id"`
+	Avatar     *Media         `db:"-" json:"avatar"`
+	AvatarJson types.JSONText `db:"avatar" json:"-"`
+
+	CoverID   *uuid.UUID     `db:"cover_id" json:"cover_id"`
+	Cover     *Media         `db:"-" json:"cover"`
+	CoverJson types.JSONText `db:"cover" json:"-"`
+
+	ReferredBy *uuid.UUID `db:"referred_by" json:"referred_by"`
+
+	IdentityVerifiedAt *time.Time `db:"identity_verified_at" json:"identity_verified_at"`
+	EmailVerifiedAt    *time.Time `db:"email_verified_at" json:"email_verified_at"`
+	PhoneVerifiedAt    *time.Time `db:"phone_verified_at" json:"phone_verified_at"`
+
+	CreatedAt time.Time  `db:"created_at" json:"created_at"`
+	UpdatedAt time.Time  `db:"updated_at" json:"updated_at"`
+	DeletedAt *time.Time `db:"deleted_at" json:"deleted_at"`
+}
 
 type ImpactPoint struct {
 	UserID              uuid.UUID      `json:"user_id" form:"user_id" validate:"required"`
@@ -39,8 +85,8 @@ func (t *SessionToken) GetUserProfile() (*User, error) {
 }
 
 // Create user from another platform (using email, firstname and lastname)
-func CreateUser(user User) (*User, error) {
-	data, _ := json.Marshal(user)
+func (u *User) CreateUser() error {
+	data, _ := json.Marshal(u)
 	body := map[string]any{}
 
 	json.Unmarshal(data, &body)
@@ -54,14 +100,10 @@ func CreateUser(user User) (*User, error) {
 		Body:     body,
 	})
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	result := new(User)
-	if err := json.Unmarshal(response, result); err != nil {
-		return nil, err
-	}
-	return result, nil
+	return json.Unmarshal(response, u)
 }
 
 // Get User profile base on access token given
